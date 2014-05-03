54bfacba83dbed1dd46d5a14587e3596c42c82be
statecollege <davesmccarthy@gmail.com>
2014-05-03 02:53:15 -0700

    boom

--- index.php
+++ index.php
@@ -12,12 +12,11 @@
 		<section id="section1">
 			<div class="container">
 				<div class="hsContainer">
-		    		<div class="hsContent" sty data-0="top: 20vh; opacity: 1;" data-500="top: 5vh; opacity: 0;">
+		    		<div class="hsContent" data-0="top: 20vh; opacity: 1;" data-425="top: 5vh; opacity: 0;">
 						<img src="img/logo.svg" alt="">
 						<h3>UI & UX Designer</h3>
 		    		</div>
 	        	</div>
-	        	<div id="diamond" data-0="top: 45vh" data-300="top: 30vh;"></div>
 			</div>
 		</section>
 		<section id="section2">
@@ -25,10 +24,20 @@
 				<div class="portfolio-group">
 					<div class="row">
 						<div class="col-md-4 portfolio-piece">
-							<div class="portfolio-pic" style="background-image: url('img/osx_promo.png')"></div>
+							<div class="portfolio-pic" style="background-image: url('img/osx_promo.png')">
+								<div class="hover-display" onclick="window.location = 'newurl.php';">
+									<h3>OS X 10.10 "Everest"</h3>
+									<p>A conceptual redesign of the current Mac OS that shows what it could look like soon.</p>
+								</div>
+							</div>
 						</div>
 						<div class="col-md-4 portfolio-piece">
-							<div class="portfolio-pic" style="background-position: 50%; background-image: url('img/fistbump_promo.png')"></div>
+							<div class="portfolio-pic" style="background-position: 50%; background-image: url('img/fistbump_promo.png')">
+								<div class="hover-display" onclick="window.location = 'newurl.php';">
+									<h3>Fist Bump</h3>
+									<p>An application based on the idea that it should be fast and easy to connect with friends.</p>
+								</div>
+							</div>
 						</div>
 						<div class="col-md-4 portfolio-piece">
 							<div class="portfolio-pic" style=""></div>
