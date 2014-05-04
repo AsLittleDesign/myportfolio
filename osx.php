77188bc6e6100bd696bd802cdb09c9567e35d30c
statecollege <davesmccarthy@gmail.com>
2014-05-03 21:58:32 -0700

    arrow and shit

--- osx.php
+++ osx.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Dave Scott McCarthy</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-        <meta name="keywords" content="Dave, Scott, McCarthy">
+		<?php
+			include('inc/head.php');
+		?>
 	</head>
 	<body>
 		<section id="section1" class="osx" style="background-color: #000000;">
 			<div style="width: 100%; height: 100%;" data-0="background-color:rgba(0,0,0,0);" data-500="background-color:rgba(0,0,0,.75);">
 				<div class="container">
-					<a href="/" class="nav">< Home</a>
+					<a href="/" class="nav"><div class="arrow"></div>Home</a>
 					<div class="hsContainer">
 			    		<div class="hsContent" data-0="top: 20vh; opacity: 1;" data-425="top: 5vh; opacity: 0;">
 							<h1>OS X 10.10 "Everest"</h1>
