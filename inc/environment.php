fbc0dcc5d8b7cc816ee76a5dec09f4509a8986c5
statecollege <davesmccarthy@gmail.com>
2014-05-11 10:18:30 -0700

    google analytics

--- inc/environment.php
+++ inc/environment.php
@@ -7,3 +7,15 @@
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script src="skrollr/src/skrollr.js"></script>
 <script src="scripts/main.js"></script>
+
+<!-- Google Analytics Tracking Code -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50872917-1', 'davesmdesign.com');
+  ga('send', 'pageview');
+
+</script>
