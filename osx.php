f52ec53cd673986c749a48ba4a7b304980eafe38
statecollege <davesmccarthy@gmail.com>
2014-05-11 00:07:18 -0700

    change!

--- osx.php
+++ osx.php
@@ -97,6 +97,9 @@
 					<br>
 					<br>
 					<p>Thank you for checking out my OS X 10.10 concept! If you liked it, please share it with your friends!</p>
+					<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+					<div class="fb-share-button" data-href="http://davesmdesign.com/osx.php" data-width="100" data-type="button_count"></div>
 				</div>
 				
 			</div>
