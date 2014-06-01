8b7bb7e518d14838d24e872192d51d9896303dbe
statecollege <davesmccarthy@gmail.com>
2014-06-01 00:12:16 -0700

    boom

--- uidesign.php
+++ uidesign.php
@@ -30,7 +30,7 @@
 					<div class="col-md-4 portfolio-piece">
 						<h3 class="portfolio-title">OS X 10.10 "Everest"</h3>
 						<div class="portfolio-border">
-							<div class="portfolio-pic" style="background-image: url('img/projects/osx/osxmockup.png')">
+							<div class="portfolio-pic" style="background-image: url('img/projects/osx/osx_promo.png')">
 								<a title="A concept for OS X 10.10. See the full concept in 'Projects'" rel="lightbox" href="img/projects/osx/osxmockup.png">
 									<div class="lightlink"></div>
 								</a>
@@ -38,17 +38,24 @@
 						</div>
 					</div>
 					<div class="col-md-4 portfolio-piece">
-						<h3 class="portfolio-title">More coming soon!</h3>
+						<h3 class="portfolio-title">Moto360 Watch Face Concept</h3>
 						<div class="portfolio-border">
-							<div class="portfolio-pic" style="">
-								<div class="portfolio-placeholder"></div>
+							<div class="portfolio-pic" style="background-image: url('img/uidesign/thumb/watch.png')">
+								<a title="My submission into the Moto360 watch face concept competition!" rel="lightbox" href="img/uidesign/watch.gif">
+									<div class="lightlink"></div>
+								</a>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div class="row break">
 					<div class="col-md-4 portfolio-piece">
-						<div class="portfolio-pic" style=""></div>
+						<h3 class="portfolio-title">More coming soon!</h3>
+						<div class="portfolio-border">
+							<div class="portfolio-pic" style="">
+								<div class="portfolio-placeholder"></div>
+							</div>
+						</div>
 					</div>
 					<div class="col-md-4 portfolio-piece">
 						<div class="portfolio-pic" style=""></div>
@@ -62,15 +69,5 @@
 		<?php
 			include('inc/environment.php')
 		?>
-		<script>
-			require(['skrollr'], function(skrollr){
-				var s = skrollr.init();
-				if(!isMobile()) {
-				    skrollr.init({
-				        forceHeight: false
-				    });
-				}
-			});
-        </script>
 	</body>
 </html>
